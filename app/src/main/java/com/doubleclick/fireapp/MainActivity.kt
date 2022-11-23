60316ddaab3506c480d2772ee4ed735ac11d1db8
eslam <eslamfgazy600@gmail.com>
2022-11-23 19:13:09 +0200

    Initial commit

---
+++ app/src/main/java/com/doubleclick/fireapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.doubleclick.fireapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.chrisbanes.photoview.PhotoView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
