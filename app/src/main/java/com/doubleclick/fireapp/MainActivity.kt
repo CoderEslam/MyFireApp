6955a573f1521605c3fa0afe90bcdc01575a0aa8
eslam <eslamfgazy600@gmail.com>
2022-11-23 21:33:55 +0200

    MyFireApp

--- app/src/main/java/com/doubleclick/fireapp/MainActivity.kt
+++ app/src/main/java/com/doubleclick/fireapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.doubleclick.fireapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.github.chrisbanes.photoview.PhotoView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
